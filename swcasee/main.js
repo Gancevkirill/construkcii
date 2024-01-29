d036a82cd0736f76689d836825be621c44422f4d
Gancevkirill <144214859+Gancevkirill@users.noreply.github.com>
2024-01-29 11:39:12 +0300

    Add files via upload

---
+++ swcasee/main.js
@@ -0,0 +1,101 @@
+//let a = 10;
+//if (a === 10) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+
+
+//let min = 30;
+//if (min < 15) {
+//  console.log('первая четверть');
+//} else if (min < 30) {
+//  console.log('вторая четверть');
+//} else if (min < 45) {
+//  console.log('третья четверть');
+//} else {
+//  console.log('четвертая четверть');
+//}
+
+
+//let lang = 'ru';
+//let arr;
+
+
+//if (lang === 'ru') {
+//  arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+//} else {
+//  arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//}
+
+
+//switch (lang) {
+//  case 'ru':
+//    arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+//    break;
+//  case 'en':
+//    arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//    break;
+//}
+
+//let arr = [['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'], ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']];
+
+//let a = 1;
+
+//if (a === 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+
+//if (a > 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a < 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a >= 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+
+//if (a <= 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a !== 0) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a === 'test') {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a === '1') {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+
+//let a = 2;
+//if (a > 0 && a < 5) {
+//  console.log('Верно');
+//} else {
+//  console.log('Неверно');
+//}
+//if (a === 0 || a === 2) {
+//  a += 7;
+//} else {
+//  a /= 10;
+//}
+//console.log(a);
+//let b = 3;
+//if (a <= 1 && b >= 3) {
